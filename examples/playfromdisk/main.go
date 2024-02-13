8ea54f41ab8b3c990243ec7c5baaca92c7f76396
Mathis Engelbart <mathis.engelbart@gmail.com>
2024-02-13 11:22:21 +0100

    Add send example

---
+++ examples/playfromdisk/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/mengelbart/roq"
+	"github.com/pion/rtp"
+	"github.com/pion/rtp/codecs"
+	"github.com/pion/webrtc/v3/pkg/media/ivfreader"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/qlog"
+)
+
+func main() {
+	videoFileName := flag.String("file", "output.ivf", "IVF video file to read")
+	addr := flag.String("addr", "localhost:443", "address to connect and send video to")
+	flag.Parse()
+
+	file, openErr := os.Open(*videoFileName)
+	if openErr != nil {
+		panic(openErr)
+	}
+
+	_, header, openErr := ivfreader.NewWith(file)
+	if openErr != nil {
+		panic(openErr)
+	}
+
+	// Determine video codec
+	var payloader rtp.Payloader
+	switch header.FourCC {
+	case "AV01":
+		payloader = &codecs.AV1Payloader{}
+	case "VP90":
+		payloader = &codecs.VP9Payloader{}
+	case "VP80":
+		payloader = &codecs.VP8Payloader{}
+	default:
+		panic(fmt.Sprintf("Unable to handle FourCC %s", header.FourCC))
+	}
+
+	packetizer := rtp.NewPacketizer(1200, 96, 1, payloader, rtp.NewRandomSequencer(), 90_000)
+
+	file, ivfErr := os.Open(*videoFileName)
+	if ivfErr != nil {
+		panic(ivfErr)
+	}
+
+	ivf, header, ivfErr := ivfreader.NewWith(file)
+	if ivfErr != nil {
+		panic(ivfErr)
+	}
+
+	conn, err := quic.DialAddr(context.Background(), *addr, &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"rtp-mux-quic-07"},
+	}, &quic.Config{
+		EnableDatagrams: true,
+		Tracer:          qlog.DefaultTracer,
+	})
+	if err != nil {
+		panic(err)
+	}
+	session, err := roq.NewSession(roq.NewQUICGoConnection(conn))
+	if err != nil {
+		panic(err)
+	}
+
+	flow, err := session.NewSendFlow(0)
+	if err != nil {
+		panic(err)
+	}
+	stream, err := flow.NewSendStream(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	ticker := time.NewTicker(time.Millisecond * time.Duration((float32(header.TimebaseNumerator)/float32(header.TimebaseDenominator))*1000))
+	for ; true; <-ticker.C {
+		frame, _, ivfErr := ivf.ParseNextFrame()
+		if errors.Is(ivfErr, io.EOF) {
+			fmt.Printf("All video frames parsed and sent")
+			os.Exit(0)
+		}
+
+		if ivfErr != nil {
+			panic(ivfErr)
+		}
+
+		packets := packetizer.Packetize(frame, 1)
+		for _, pkt := range packets {
+			buf, err := pkt.Marshal()
+			if err != nil {
+				panic(err)
+			}
+			_, err = stream.WriteRTPBytes(buf)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	stream.Close()
+}
--- go.mod
+++ go.mod
@@ -2,17 +2,23 @@ module github.com/mengelbart/roq
 
 go 1.21.1
 
-require github.com/quic-go/quic-go v0.41.0
+require (
+	github.com/pion/rtp v1.8.3
+	github.com/pion/webrtc/v3 v3.2.28
+	github.com/quic-go/quic-go v0.41.0
+)
 
 require (
+	github.com/francoispqt/gojay v1.2.13 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
 	go.uber.org/mock v0.3.0 // indirect
-	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
 	golang.org/x/mod v0.11.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/tools v0.9.1 // indirect
 )
